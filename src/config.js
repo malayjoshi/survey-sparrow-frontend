439167e48ff4599ebb3b6b25bd142322bbe69ff7
malayjoshi <malayace.joshi@gmail.com>
2021-03-26 21:04:43 +0530

    my version

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 module.exports = global.config = {
-<<<<<<< HEAD
+
     apiPath: "http://139.59.23.150:8080" ,
-=======
-    apiPath: "//139.59.23.150:8080",
->>>>>>> 591098c96618f7415881c787ef5bf0be030f6053
+
+   
     timeInterval:60*5*1000
     // other global config variables you wish
 };
