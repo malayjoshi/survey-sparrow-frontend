e595e888bdb2963f1e842e9290993c0cc87e1001
malayjoshi <malayace.joshi@gmail.com>
2021-03-26 21:00:48 +0530

    changed api path to ip:3000

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = global.config = {
-    apiPath: "http://localhost:8080",
+    apiPath: "http://139.59.23.150:8080" ,
     timeInterval:60*5*1000
     // other global config variables you wish
 };
