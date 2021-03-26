6e813d97dd2316df838378f8b7048aaa5ca407d2
malayjoshi <malayace.joshi@gmail.com>
2021-03-26 21:28:31 +0530

    changed api path to production version

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = global.config = {
 
     apiPath: "http://139.59.23.150:8080" ,
-
+    //apiPath: "http://localhost:8080" ,
    
     timeInterval:60*5*1000
     // other global config variables you wish
